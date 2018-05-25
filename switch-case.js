7b72d1b4c0d31259660942c9474a53219605fcd1
caktiibrahim <caktiibrahim@hotmail.com>
2018-05-26 03:01:40 +0700

    tugas SwitchCase

---
+++ switch-case.js
@@ -0,0 +1,57 @@
+var tanggal = 25
+var bulan = 12
+var tahun = 2018
+bulanDesc = ''
+
+switch (bulan){
+  case 1 : {
+    bulanDesc = 'Januari';
+    console.log(tanggal + ' Januari ' + tahun);
+    break;
+  }case 2 : {
+    bulanDesc = 'Februrari';
+    console.log(tanggal + ' February ' + tahun);
+    break;
+  }case 3 : {
+    bulanDesc = 'Maret';
+    console.log(tanggal + ' Maret ' + tahun);
+    break;
+  }case 4 : {
+    bulanDesc = 'April';
+    console.log(tanggal + ' April '+ tahun);
+    break;
+  }case 5 : {
+    bulanDesc = 'Mei';
+    console.log(tanggal + ' Mei ' + tahun);
+    break;
+  }case 6 : {
+    bulanDesc = 'Juni';
+    console.log(tanggal + ' Juni ' + tahun);
+    break;
+  }case 7 : {
+    bulanDesc = 'Juli';
+    console.log(tanggal + ' Juli ' + tahun);
+    break;
+  }case 8 :{ 
+    bulanDesc = 'Agustus';
+    console.log(tanggal + ' Agustus ' + tahun);
+    break;
+  }case 9 : {
+    bulanDesc = 'September';
+    console.log(tanggal + ' September ' + tahun);
+    break;
+  }case 10: {
+    bulanDesc = 'Oktober';
+    console.log(tanggal + ' Oktober '+ tahun);
+    break;
+  }case 11:{
+    bulanDesc = 'Nopember';
+    console.log(tanggal + ' Nopember ' + tahun);
+    break;
+  }case 12:{
+    bulanDesc = 'Desember';
+    console.log(tanggal + ' Desember '+ tahun)
+  }break;
+}
+
+

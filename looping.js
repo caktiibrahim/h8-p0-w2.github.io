c34f35fcef76a7ccd7c01dcd1d80013c8d1abc92
caktiibrahim <caktiibrahim@hotmail.com>
2018-05-27 01:39:11 +0700

    upload tugas looping

--- looping.js
+++ looping.js
@@ -25,22 +25,3 @@ for(var i = 20; i > 0; i--){
 }
 
 console.log('==================================')
-
-var nama = 'cakti'
-var peran = 'ksatria'
-
-if(nama === '' && peran === ''){
-  console.log('Nama harus di isi!')
-}else if(nama === 'cakti' && peran === ''){
-  console.log('Selamat datang di dunia Proxytia, ' + nama + '.');
-  console.log('Halo ' + nama + ',' + ' pilih peranmu untuk memulai game!');
-}else if(peran === 'ksatria'){
-  console.log('Selamat datang di Dunia Proxytia, ' + nama);
-  console.log('Halo Ksatria ' + nama + ',' + ' kamu dapat menyerang dengan senjatamu!');
-}else if(peran === 'tabib'){
-  console.log('Selamat datang di Dunia Proxytia, ' + nama);
-  console.log('Halo tabib ' + nama + ',' + ' kamu dapat menolong teman mu yang terluka');
-}else if(peran === 'penyihir'){
-  console.log('Selamat datang di Dunia Proxytia, ' + nama);
-  console.log('Halo penyihir ' + nama + ',' + ' ciptakan keajaiban untuk menyerang musuh-musuh mu!');
-}

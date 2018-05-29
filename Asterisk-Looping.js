4270e96fef48d39cab9800fb833147f2a3656d40
caktiibrahim <caktiibrahim@hotmail.com>
2018-05-30 02:19:32 +0700

    upload ulang'
    clear
    '

---
+++ Asterisk-Looping.js
@@ -0,0 +1,33 @@
+var rows1 = 5
+
+for(init = 1; init <= rows1 ; init++ ){
+  print = '*'
+  console.log(print);
+}
+
+
+console.log('=============================');
+var rows2 = 5
+
+for (ii = 0; ii  < rows2; ii++){
+  var print = ''
+  for (jj = 0; jj < rows2; jj++){
+    print = print + '*'
+  }
+      console.log(print)
+}
+
+
+
+
+console.log('=============================');
+
+var rows3 = 5
+var coloumn3 = 5
+var print = ''
+
+for(i = 0; i < rows3; i++){
+  for(j = 0; j < coloumn3; j++);
+  print = print +'*'
+  console.log(print);
+}
